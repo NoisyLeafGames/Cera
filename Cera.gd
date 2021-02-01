2c6c30354d2df68240e36fcde2fedb00881ba470
dnadan2 <_dan_@live.com>
2021-02-01 23:30:58 +0000

    collisions and timed features

--- Cera.gd
+++ Cera.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 var target = Vector2(500,1100)
 var speed = 5
@@ -13,14 +13,20 @@ func _input(event):
 func _process(delta):
 	var velocity = (target - position).normalized() * speed
 	
-	if (target - position).length() > 5:
-		get_node("AnimationPlayer").play("run")
+	if (target - position).length() > 10:
 		
-		position += velocity
+		var collision = move_and_collide(velocity)
+		
+		if collision != null:
+			get_node("AnimationPlayer").play("stand")
+		else:
+			get_node("AnimationPlayer").play("run")
+		
+		if velocity.x < 0:
+			rotation = 0
+			scale = Vector2(-0.3, 0.3)
+		else:
+			rotation = 0
+			scale = Vector2(0.3, 0.3)
 	else:
 		get_node("AnimationPlayer").play("stand")
-	
-	if velocity.x < 0:
-		set_scale(Vector2(-0.3,0.3))
-	else:
-		set_scale(Vector2(0.3,0.3))
--- level.gd
+++ level.gd
@@ -2,12 +2,18 @@ extends Node2D
 
 const food = preload("res://food.tscn")
 var foodsNode = null
-var foodAreaSize
+var foodAreaSize = Vector2(-1,-1)
 
-var lastFoodAdded = 
-
+const foodRefeshTime = 5000
 const halfFoodSize = Vector2(128,128)
-const maxFoodCount = 3
+const maxFoodCount = 4
+
+var lastFoodSpawned = OS.get_ticks_msec()
+var lastEaten = OS.get_ticks_msec()
+var lastDrunk = OS.get_ticks_msec()
+var lastSlept = OS.get_ticks_msec()
+
+var lastTick = OS.get_ticks_msec()
 
 func _ready():
 	randomize()
@@ -15,9 +21,24 @@ func _ready():
 	foodAreaSize = get_node("grass/grass").get_rect().size - halfFoodSize
 
 func _process(delta):
-	if foodsNode.get_child_count() <= maxFoodCount:
-		var foodToAdd = food.instance()
+	var currentTick = OS.get_ticks_msec()
+
+	if lastTick + 100 < currentTick:
+		lastTick = currentTick
 		
-		foodToAdd.position = Vector2(randf(),randf()) * foodAreaSize
+		if lastEaten + 5000 < currentTick:
+			get_node("Bars/Food").value -= 0.1
+		if lastDrunk + 5000 < currentTick:
+			get_node("Bars/Drink").value -= 0.1
+		if lastSlept + 5000 < currentTick:
+			get_node("Bars/Sleep").value -= 0.1
+
+	if lastFoodSpawned + foodRefeshTime < currentTick:
+		lastFoodSpawned = currentTick
 		
-		foodsNode.add_child(foodToAdd)
+		if foodsNode.get_child_count() < maxFoodCount:
+			var foodToAdd = food.instance()
+			
+			foodToAdd.position = Vector2(randf(),randf()) * foodAreaSize
+			
+			foodsNode.add_child(foodToAdd)
